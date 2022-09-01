348c6869179cc5d60ba166a4284b98b004bfb90f
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-09-01 20:26:59 +0530

    Add files via upload

---
+++ myProgram.java
@@ -0,0 +1,10 @@
+package Default;
+
+//Q.1 Fix the compilation error and run the program 
+public class myProgram {
+
+	public static void main(String args[]) { // We don't use ";" at the method declaring line
+		System.out.println("My first Java Program");
+	}
+
+}
