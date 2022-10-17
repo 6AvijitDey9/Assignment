3db73c3dd93733b8bd0cac4f1779111357713f5d
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-10-17 14:33:35 +0530

    Add files via upload

---
+++ Calculate the area of rectangle/AreaCalculator.java
@@ -0,0 +1,10 @@
+package com.cognizant.shapes; //Given package name according to the question
+
+public class AreaCalculator { //Develop another class named "AreaCalculator"
+
+	public static void main(String[] args) {
+		Rectangle shape=new Rectangle(); //Calling the first class to display the message
+		shape.calculateArea();
+	}
+
+}
